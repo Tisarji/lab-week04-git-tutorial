052821cade42a2b15494c782a4c1b1d22644ad12
Jirasit <48820052+TONG2K@users.noreply.github.com>
2024-07-12 16:23:24 +0700

    :zap: Init anything about week

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php echo "Hello World!"; ?>
+<?php echo "My Name is Jirasit"; ?>
+<?php echo "I am a Web Developer"; ?>
+<?php echo "I am a PHP Developer"; ?>
+<?php echo "I am a Java Developer"; ?>
+<?php echo "Dont Do that!"; ?>
---
+++ menu.php
@@ -0,0 +1,9 @@
+<?php echo "Main";
+echo "Menu-1";
+echo "Contact-2";
+echo "About-3";
+echo "Service-4";
+echo "Product-5";
+echo "Gallery-6";
+echo "FAQ-7";
+?>
